c61429b187b906f4fb4776b7ee787e034cecfe37
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2023-02-22 17:31:13 -0600

    begun styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./style.css";
 import { getWeather, getGiphy } from "./sect/api.js";
 import "./sect/gif";
-import gifTemp from "./sect/gif";
+// import gifTemp from "./sect/gif";
 
 const img = document.querySelector('img');
 const para = document.querySelector('p');
 
 getWeather('houston', 'f', para);
 getGiphy(img, 'cold');
-gifTemp(250);
+// gifTemp(250);
