b98975f83f7d544db82b95b4f77576c2b3a8cfea
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2023-02-23 14:59:05 -0600

    still working with the apis

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import "./style.css";
 import { getWeather, getGiphy } from "./sect/api.js";
 import "./sect/gif";
-import "./sect/convert"
+import "./sect/convert";
 import { kToF } from "./sect/convert";
 import "./sect/gif";
+import gifTemp from "./sect/gif";
 
 const p = document.querySelector('p');
 const img = document.querySelector('img');
 
 const weather = getWeather('Houston');
+// const testGif = getGiphy('hot');
+const weatherGif = getGiphy(weather)
+// const weatherGif = getGiphy(weather.then((weatherData) => console.log(gifTemp(weatherData.main.temp))));
+
 weather.then((weatherData) => p.textContent = (kToF(weatherData.main.temp)));
 
 
-getGiphy(img, 'cold');
+weatherGif.then((gif) => img.src = gif.data.images.original.url);
+
+// testGif.then((gif) => img.src = gif.data.images.original.url)
--- src/sect/api.js
+++ src/sect/api.js
@@ -6,12 +6,6 @@ async function getWeather (city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfb21599f58601e283f09e7e87e18051`, {mode:"cors"})
     const weatherData = await response.json();
     return weatherData
-
-    // if (scale === 'f') {
-    //     el.textContent = Math.round(kToF(currentWeather));
-    // } else if (scale === 'c') {
-    //     el.textContent = Math.round(kToC(currentWeather));
-    // }
 }
 
 async function getGiphy (temp) {
